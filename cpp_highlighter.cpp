e4655e2ce35b7f08f1871704f5c9a3e5d6d3bb5e
Ildar Manashev <imanashev@gmail.com>
2019-05-09 01:52:33 +0700

    [-] ';' from keywords

--- cpp_highlighter.cpp
+++ cpp_highlighter.cpp
@@ -252,7 +252,7 @@ bool CPPHighlighter::isKeyword(std::string &str)
         "or_eq", "private", "protected", "public", "register", "reinterpret_cast", "return", "short", "signed",
         "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "template", "this",
         "thread_local", "throw", "true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using",
-        "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq", ";"
+        "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq"
     };
 
     return keywords.find(str) != keywords.end();
