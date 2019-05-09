32eba87da735fe5f68f4ccf9d7e8989f880ad687
Ildar Manashev <imanashev@gmail.com>
2019-05-10 00:24:40 +0700

    [+] black theme
    add background color configuration

--- colors.h
+++ colors.h
@@ -6,8 +6,13 @@ constexpr char string[] = "008000";
 constexpr char number[] = "87bdd8";
 constexpr char comment[] = "696969";
 constexpr char keyword[] = "FF8C00";
+constexpr char default_text[] = "DCDCDC";
 constexpr char preproc[] = "FFD700";
 constexpr char lines[] = "A9A9A9";
-constexpr char background[] = "FFFFFF";
+constexpr char background[] = "202020";
+
+// rgb 0..255
+constexpr int lower_bound = 50;
+constexpr int upper_bound = 255;
 
 }
--- html_wrapper.cpp
+++ html_wrapper.cpp
@@ -6,7 +6,8 @@ std::string &HTMLWrapper::process(std::string &str)
 {
     std::cout << "HTMLDecorator process"<< std::endl;
 
-    const std::string head = "<html>\n<body bgcolor=" + std::string(color::background) + ">\n<pre>\n";
+    const std::string head = "<html>\n<body bgcolor=" + std::string(color::background) +
+                             " text=" + std::string(color::default_text) + ">\n<pre>\n";
     const std::string tail{"\n</pre>\n</body>\n</html>\n"};
 
     str = head + str + tail;
